ff9a881000ab7f2f220902065ed9fb47b410e740
Fabrice7506 <sieguy7506@gmail.com>
2024-09-03 21:05:49 +0000

    first

--- view/classe.php
+++ view/classe.php
@@ -53,7 +53,7 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                    Voulez vous supprimer la classe de <?= $classe['lib_classe'] ?> ?
+                    Voulez vous supprimer la classe de la <?= $classe['lib_classe'] ?> ?
 
                     </div>
                     <div class="modal-footer">
